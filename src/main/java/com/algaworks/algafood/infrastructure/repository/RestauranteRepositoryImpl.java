545231d40d60f5c30cf77d9b2bc67658308f2936
joseMarciano <marcianojosepaulo@gmail.com>
2021-11-02 13:41:40 -0300

    - 5.19. *Injetando o próprio repositório na implementação customizada e a anotação @Lazy*

--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -2,11 +2,18 @@ package com.algaworks.algafood.infrastructure.repository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.CustomizeRestauranteRepository;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+import com.algaworks.algafood.infrastructure.repository.spec.RestauranteSpecs;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.math.BigDecimal;
 import java.util.List;
+
+import static com.algaworks.algafood.infrastructure.repository.spec.RestauranteSpecs.comFreteGratis;
+import static com.algaworks.algafood.infrastructure.repository.spec.RestauranteSpecs.comNomeSemelhante;
 
 /*
  * Um repositório customizado não precisa implementar o contrato RestauranteRepository desde que tenha o Sufixo IMPL
@@ -16,6 +23,10 @@ import java.util.List;
 public class RestauranteRepositoryImpl implements CustomizeRestauranteRepository {
 
     private final EntityManager em;
+
+    @Autowired
+    @Lazy //Usando essa anotação para evitar redundancia ciclica
+    private RestauranteRepository restauranteRepository;
 
     public RestauranteRepositoryImpl(EntityManager em) {
         this.em = em;
@@ -31,5 +42,10 @@ public class RestauranteRepositoryImpl implements CustomizeRestauranteRepository
                 .setParameter("taxaFinal", taxaFreteFinal)
                 .getResultList();
 
+    }
+
+    @Override
+    public List<Restaurante> findComFreteGratis(String nome) {
+        return restauranteRepository.findAll(comFreteGratis().and(comNomeSemelhante(nome)));
     }
 }
