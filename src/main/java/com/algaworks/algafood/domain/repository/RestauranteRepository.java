65265d876fb319223cd39dd811ea3ca83e0b1da9
joseMarciano <marcianojosepaulo@gmail.com>
2021-11-02 13:05:19 -0300

    - 5.17. *Implementando Specifications com SDJ*

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -2,7 +2,10 @@ package com.algaworks.algafood.domain.repository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long>, CustomizeRestauranteRepository {
+public interface RestauranteRepository extends JpaRepository<Restaurante, Long>,
+        CustomizeRestauranteRepository,
+        JpaSpecificationExecutor<Restaurante> {
 
 }
