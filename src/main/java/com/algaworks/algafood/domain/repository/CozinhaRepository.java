e34ad0986e6752712f403193d16138108614a4a8
joseMarciano <marcianojosepaulo@gmail.com>
2021-11-02 11:04:17 -0300

    -5.9. *Usando queries JPQL customizadas com @Query*

--- src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -2,11 +2,16 @@ package com.algaworks.algafood.domain.repository;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 
-//    List<Cozinha> consultarPorNome(String nome);
+    @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+    List<Cozinha> consultarPorNome(String nome, @Param("id") Long cozinha);
 
 }
