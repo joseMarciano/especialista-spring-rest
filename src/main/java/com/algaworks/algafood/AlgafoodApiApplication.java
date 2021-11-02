e7cf0363d55ac4b286117e384f713ac52ce752c6
joseMarciano <marcianojosepaulo@gmail.com>
2021-11-02 14:06:18 -0300

    - 5.20. *Estendendo o JpaRepository para customizar o repositório base*

--- src/main/java/com/algaworks/algafood/AlgafoodApiApplication.java
+++ src/main/java/com/algaworks/algafood/AlgafoodApiApplication.java
@@ -1,13 +1,19 @@
 package com.algaworks.algafood;
 
+import com.algaworks.algafood.infrastructure.repository.CustomJpaRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+// Altera a classe padrão de implementação das querys... antes era SimpleJpaRepository.
+// OBS: a classe informada agora extende o SimpleJpaRepository então não muda muito alem de
+// criarmos mais métodos padrão para todos os repositorios
+@EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class AlgafoodApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AlgafoodApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AlgafoodApiApplication.class, args);
+    }
 
 }
--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -1,10 +1,9 @@
 package com.algaworks.algafood.domain.repository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long>,
+public interface RestauranteRepository extends CustomJpaRepository<Restaurante, Long>,
         CustomizeRestauranteRepository,
         JpaSpecificationExecutor<Restaurante> {
 
