a183ba42faaa81455f317fb7a985782305030cbd
joseMarciano <marcianojosepaulo@gmail.com>
2021-11-14 11:25:47 -0300

    - 8.14. *Desafio: implementando exception handler* *Ajuste em FK de cidades pois estava errado*

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.algaworks.algafood.api.exceptionhandler;
 
 
+import com.algaworks.algafood.api.exceptionhandler.Problema.ProblemaBuilder;
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.NegocioException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,13 +18,38 @@ public class ApiExceptionHandler {
     @ExceptionHandler({EntidadeNaoEncontradaException.class})
     public ResponseEntity<?> handleEstadoNaoEncontradoException(EntidadeNaoEncontradaException e) {
 
-        Problema problema = Problema.builder()
-                .dataHora(LocalDateTime.now())
+        Problema problema = problemadefaultBuilder()
                 .mensagem(e.getMessage())
                 .build();
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problema);
     }
 
+    @ExceptionHandler({NegocioException.class})
+    public ResponseEntity<?> handleNegocioException(NegocioException e) {
+
+        Problema problema = problemadefaultBuilder()
+                .mensagem(e.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
+    }
+
+    @ExceptionHandler({EntidadeEmUsoException.class})
+    public ResponseEntity<?> handleEntidadeEmUsoException(EntidadeEmUsoException e) {
+
+        Problema problema = problemadefaultBuilder()
+                .mensagem(e.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(problema);
+    }
+
+
+    private ProblemaBuilder problemadefaultBuilder() {
+        return Problema.builder()
+                .dataHora(LocalDateTime.now())
+                .mensagem("Ocorreu um erro interno no servidor");
+    }
 
 }
