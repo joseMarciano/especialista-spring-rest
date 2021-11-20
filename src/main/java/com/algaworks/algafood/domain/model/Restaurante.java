ab91b471d2909be6667d4bf53081dc7324f6d2b9
joseMarciano <marcianojosepaulo@gmail.com>
2021-11-20 12:16:22 -0300

    - 9.5. *Conhecendo e adicionando mais constraints de validação no modelo*

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -7,7 +7,10 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -24,10 +27,14 @@ public class Restaurante {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull(message = "É obrigatório informar o nome")
+    @NotBlank(message = "É obrigatório informar o nome")
+//    @NotNull
+//    @NotEmpty
     @Column(name = "NOME")
     private String nome;
 
+    //    @DecimalMin(value = "1", message = "O valor mínimo da taxa de frete é {value}")
+    @PositiveOrZero(message = "O valor da taxa frete deve ser maior do que zero")
     @Column(name = "TAXA_FRETE")
     private BigDecimal taxaFrete;
 
