392dd08dfa30f105cc32416a522d5fa12e546d2a
joseMarciano <marcianojosepaulo@gmail.com>
2021-11-29 23:27:08 -0300

    - 10.8. *Implementando Testes de API com REST Assured e validando o código de status HTTP*

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -4,49 +4,65 @@ import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradoException;
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.service.CozinhaService;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.validation.ConstraintViolationException;
 
+import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ExtendWith(SpringExtension.class)
 class CadastroCozinhaIT {
 
-    @Autowired
-    private CozinhaService service;
-
-
-    @Test
-    public void testarCadastroCozinhaComSucesso() {
-        Cozinha cozinha = new Cozinha();
-        cozinha.setNome("Cozinha teste");
-
-        cozinha = service.save(cozinha);
-
-        assertNotNull(cozinha);
-        assertNotNull(cozinha.getId());
-    }
+    @LocalServerPort
+    private int port;
 
     @Test
-    public void testarCadastroCozinhaSemNome() {
-        assertThrows(ConstraintViolationException.class, () -> service.save(new Cozinha()));
+    public void deveRetornarStatus200QuandoConsultarCozinhas() {
+        given()
+                .basePath("/cozinhas")
+                .port(port)
+                .accept(ContentType.JSON)
+                .when().get().then().statusCode(HttpStatus.OK.value());
     }
-
-    @Test
-    public void deveFalharQuandoExcluirCozinhaEmUso() {
-        assertThrows(EntidadeEmUsoException.class, () -> service.remove(1L));
-    }
-
-    @Test
-    public void deveFalharQuandoExcluirCozinhaInexistente() {
-        assertThrows(CozinhaNaoEncontradoException.class, () -> service.remove(9999L));
-    }
+//    @Autowired
+//    private CozinhaService service;
+//
+//
+//    @Test
+//    public void testarCadastroCozinhaComSucesso() {
+//        Cozinha cozinha = new Cozinha();
+//        cozinha.setNome("Cozinha teste");
+//
+//        cozinha = service.save(cozinha);
+//
+//        assertNotNull(cozinha);
+//        assertNotNull(cozinha.getId());
+//    }
+//
+//    @Test
+//    public void testarCadastroCozinhaSemNome() {
+//        assertThrows(ConstraintViolationException.class, () -> service.save(new Cozinha()));
+//    }
+//
+//    @Test
+//    public void deveFalharQuandoExcluirCozinhaEmUso() {
+//        assertThrows(EntidadeEmUsoException.class, () -> service.remove(1L));
+//    }
+//
+//    @Test
+//    public void deveFalharQuandoExcluirCozinhaInexistente() {
+//        assertThrows(CozinhaNaoEncontradoException.class, () -> service.remove(9999L));
+//    }
 
 }
