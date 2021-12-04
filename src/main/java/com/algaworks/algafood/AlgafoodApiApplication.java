814f153cbfe88c1e6dcb541d53ad43357d6e1c54
joseMarciano <marcianojosepaulo@gmail.com>
2021-12-04 13:28:46 -0300

    - 11.7. *Configurando e refatorando o projeto para usar UTC*

--- src/main/java/com/algaworks/algafood/AlgafoodApiApplication.java
+++ src/main/java/com/algaworks/algafood/AlgafoodApiApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.sql.Time;
+import java.util.TimeZone;
+
 @SpringBootApplication
 // Altera a classe padrão de implementação das querys... antes era SimpleJpaRepository.
 // OBS: a classe informada agora extende o SimpleJpaRepository então não muda muito alem de
@@ -13,6 +16,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class AlgafoodApiApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC")); // retorna sempre em padrão UTC (com offset 0)... fica cargo do consumidor converter
         SpringApplication.run(AlgafoodApiApplication.class, args);
     }
 
--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -15,6 +15,7 @@ import javax.validation.groups.ConvertGroup;
 import javax.validation.groups.Default;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -64,10 +65,10 @@ public class Restaurante {
 
     @CreationTimestamp
     @Column(name = "DATA_CADASTRO", columnDefinition = "DATETIME(0)")
-    private LocalDateTime dataCadastro;
+    private OffsetDateTime dataCadastro;
 
     @UpdateTimestamp
     @Column(name = "DATA_ATUALIZACAO", columnDefinition = "DATETIME(0)")
-    private LocalDateTime dataAtualizacao;
+    private OffsetDateTime dataAtualizacao;
 
 }
