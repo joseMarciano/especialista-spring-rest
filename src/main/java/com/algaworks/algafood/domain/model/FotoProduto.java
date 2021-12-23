91b8955862e20cb1b1d20b95aeda1f84eee73056
joseMarciano <marcianojosepaulo@gmail.com>
2021-12-22 23:03:14 -0300

    - 14.5. *Mapeando entidade FotoProduto e relacionamento um-para-um*

---
+++ src/main/java/com/algaworks/algafood/domain/model/FotoProduto.java
@@ -0,0 +1,34 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "FOTOS_PRODUTOS")
+public class FotoProduto {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @Column(name = "PRODUTOS_ID")
+    private Long id;
+
+    @Column(name = "NOME")
+    private String nome;
+
+    @Column(name = "DESCRICAO")
+    private String descricao;
+
+    @Column(name = "CONTENT_TYPE")
+    private String contentType;
+
+    @Column(name = "TAMANHO")
+    private Long tamanho;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    private Produto produto;
+}
